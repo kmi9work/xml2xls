d9394f14ad9c36bdd3af10978ef2aebb97f42aeb
Михаил Костенчук <MichaelKostekchuk@iMac-iMac.local>
2011-04-28 21:15:21 +0400

    empty

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Xml2xls::Application.config.secret_token = '619432908c16eaf2731824e679fa37a908277a5f022ccaab6bff24ebe5d89971d005fcdb2f3d588a15c73fca9bfe7bac44e7830f184f83e1978f7d906d56ab38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Xml2xls::Application.config.session_store :cookie_store, :key => '_xml2xls_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Xml2xls::Application.config.session_store :active_record_store
