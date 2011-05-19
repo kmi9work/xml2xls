26b059dc6c08f910519d5bd3114ebeb31457a356
Михаил Костенчук <MichaelKostekchuk@MacBookMK-7.local>
2011-05-19 20:50:19 +0400

    univer

--- vendor/our_parser/init.rb
+++ vendor/our_parser/init.rb
@@ -4,4 +4,5 @@ require 'rubygems'
 require './lib/our_parser.rb'
 #Debugger.start
 xls = OurParser.new(File.read(ARGV[0]), ARGV[1])
-puts xls.out_xml.to_s
+p xls.out_xml
+puts xls.out_xml.write("spreadsheet.xls")
